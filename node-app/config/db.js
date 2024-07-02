2e03e1d0b7b1346737fcdc8c6e9b3047da8ea439
M0Aziz <medaziz.chouchi@esprit.tn>
2024-07-02 19:01:41 +0200

    Update DB Config commit

--- node-app/config/db.js
+++ node-app/config/db.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
- 
   try {
-   /* await mongoose.connect('mongodb://localhost:27017/declic', {
+    await mongoose.connect('mongodb://mongodb:27017/declic', {
       useNewUrlParser: true,
       useUnifiedTopology: true
-    });*/
-
-   await mongoose.connect('mongodb://atlas-sql-662a6549e6044373f2acb35d-sbven.a.query.mongodb.net/declic?ssl=true&authSource=admin')
+    });
     console.log('Connexion à MongoDB réussie');
   } catch (error) {
     console.error('Erreur lors de la connexion à MongoDB:', error.message);
